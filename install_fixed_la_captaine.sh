17ddef89f35f43162d702b2d1223d156025c1239
surajmandalcell <surajmandalcell@gmail.com>
2018-04-21 15:37:40 +0530

    add la captaine wingpanel fix script

---
+++ install_fixed_la_captaine.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/keeferrourke/la-capitaine-icon-theme.git ~/.icons/la-captaine
+cd ~/.icons/la-captaine
+# Fix wingpanel icons
+cp -r /usr/share/icons/elementary/status/24/* panel/24/
