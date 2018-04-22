92aeb79eebb648d0ca4dd21215443f299225e703
surajmandalcell <surajmandalcell@gmail.com>
2018-04-22 15:04:02 +0530

    fixed power button too

--- install_fixed_la_captaine.sh
+++ install_fixed_la_captaine.sh
@@ -2,3 +2,6 @@ git clone https://github.com/keeferrourke/la-capitaine-icon-theme.git ~/.icons/l
 cd ~/.icons/la-captaine
 # Fix wingpanel icons
 cp -r /usr/share/icons/elementary/status/24/* panel/24/
+# Fix wingpanel power button
+rm -rf /panel/24/system-devices-panel.svg
+sed -i -e 's/breeze,//g' index.theme
