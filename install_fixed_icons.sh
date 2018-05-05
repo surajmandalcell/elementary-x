7e5b0fe5c9efa7e190761f4d5f8dbb6ccb67eaaa
surajmandalcell <surajmandalcell@gmail.com>
2018-05-06 00:18:11 +0530

    fix certain newbie things

--- install_fixed_icons.sh
+++ install_fixed_icons.sh
@@ -3,5 +3,6 @@ cd ~/.icons/la-captaine
 # Fix wingpanel icons
 cp -r /usr/share/icons/elementary/status/24/* panel/24/
 # Fix wingpanel power button
-rm -rf /panel/24/system-devices-panel.svg
+rm -rf ~/.icons/la-captaine/panel/24-light/system-devices-panel.svg
+rm -rf ~/.icons/la-captaine/panel/24-dark/system-devices-panel.svg
 sed -i -e 's/breeze,//g' index.theme
