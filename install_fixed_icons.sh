6d2b3ae25c14b95d3cc9cd0ee6b5a6cd5650b29f
surajmandalcell <surajmandalcell@gmail.com>
2018-05-23 12:18:45 +0530

    fixed eos big power button issue

--- install_fixed_icons.sh
+++ install_fixed_icons.sh
@@ -3,6 +3,6 @@ cd ~/.icons/la-captaine
 # Fix wingpanel icons
 cp -r /usr/share/icons/elementary/status/24/* panel/24/
 # Fix wingpanel power button
-rm -rf ~/.icons/la-captaine/panel/24-light/system-devices-panel.svg
-rm -rf ~/.icons/la-captaine/panel/24-dark/system-devices-panel.svg
+rm -rf ~/.icons/la-captaine/status/symbolic/system-shutdown-symbolic.svg
+rm -rf ~/.icons/la-captaine/actions/symbolic/system-shutdown-symbolic.svg
 sed -i -e 's/breeze,//g' index.theme
